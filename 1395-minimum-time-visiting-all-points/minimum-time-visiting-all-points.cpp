094284e121527f7f2f966f896da536cad7b2078f
Muhammad Subhan <138607323+Subhan0309@users.noreply.github.com>
2023-12-03 20:37:34 +0500

    Time: 6 ms (68.54%) | Memory: 10.7 MB (14.02%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,49 +1,59 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int total_time = 0;
+    // int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+    //     int total_time = 0;
 
-        for (int i = 0; i < points.size() - 1; i++) {
-            vector<int> curr = points[i];
-            vector<int> dest = points[i + 1];
+    //     for (int i = 0; i < points.size() - 1; i++) {
+    //         vector<int> curr = points[i];
+    //         vector<int> dest = points[i + 1];
 
-            // Move diagonally
-            while (curr[0] != dest[0] && curr[1] != dest[1]) {
-                if (dest[0] > curr[0]) {
-                    curr[0]++;
-                } else if (dest[0] < curr[0]) {
-                    curr[0]--;
-                }
+    //         // Move diagonally
+    //         while (curr[0] != dest[0] && curr[1] != dest[1]) {
+    //             if (dest[0] > curr[0]) {
+    //                 curr[0]++;
+    //             } else if (dest[0] < curr[0]) {
+    //                 curr[0]--;
+    //             }
 
-                if (dest[1] > curr[1]) {
-                    curr[1]++;
-                } else if (dest[1] < curr[1]) {
-                    curr[1]--;
-                }
+    //             if (dest[1] > curr[1]) {
+    //                 curr[1]++;
+    //             } else if (dest[1] < curr[1]) {
+    //                 curr[1]--;
+    //             }
 
-                total_time++;
-            }
+    //             total_time++;
+    //         }
 
-            // Adjust horizontally or vertically
-            while (curr[0] != dest[0]) {
-                if (dest[0] > curr[0]) {
-                    curr[0]++;
-                } else {
-                    curr[0]--;
-                }
-                total_time++;
-            }
+    //         // Adjust horizontally or vertically
+    //         while (curr[0] != dest[0]) {
+    //             if (dest[0] > curr[0]) {
+    //                 curr[0]++;
+    //             } else {
+    //                 curr[0]--;
+    //             }
+    //             total_time++;
+    //         }
 
-            while (curr[1] != dest[1]) {
-                if (dest[1] > curr[1]) {
-                    curr[1]++;
-                } else {
-                    curr[1]--;
-                }
-                total_time++;
-            }
+    //         while (curr[1] != dest[1]) {
+    //             if (dest[1] > curr[1]) {
+    //                 curr[1]++;
+    //             } else {
+    //                 curr[1]--;
+    //             }
+    //             total_time++;
+    //         }
+    //     }
+
+    //     return total_time;
+    // }
+        int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int res=0;
+        for(int i=1;i<points.size();i++) {
+            int x=abs(points[i][0]-points[i-1][0]), y=abs(points[i][1]-points[i-1][1]);
+            res+=max(x, y);
         }
-
-        return total_time;
+        return res;
     }
 };
+
+
