921a86481e930e13746c110d096474fd5b78a62b
Muhammad Subhan <138607323+Subhan0309@users.noreply.github.com>
2023-12-04 22:19:05 +0500

    Time: 8 ms (5.22%) | Memory: 7.1 MB (18.84%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,41 +1,68 @@
 class Solution {
 public:
- bool checkGood_String(string str) {
-    if (str.length() != 3) {
-        return false;
-    }
+//  bool checkGood_String(string str) {
+//     if (str.length() != 3) {
+//         return false;
+//     }
 
-    if ((str[0] == str[1]) && (str[1] == str[2])) {
-        return true;
-    }
+//     if ((str[0] == str[1]) && (str[1] == str[2])) {
+//         return true;
+//     }
 
-    return false;
-}
-bool isGreator(string str1,string str2)
-{
-    return stoi(str1)>stoi(str2);
-}
+//     return false;
+// }
+// bool isGreator(string str1,string str2)
+// {
+//     return stoi(str1)>stoi(str2);
+// }
 
-string largestGoodInteger(string num) {
-    string max = "-111";
+// string largestGoodInteger(string num) {
+//     string max = "-111";
 
-    int left = 0;
-    int right = 2;
+//     int left = 0;
+//     int right = 2;
 
-    while (right < num.size()) {
+//     while (right < num.size()) {
 
-        string holder = num.substr(left, right - left + 1);
+//         string holder = num.substr(left, right - left + 1);
 
-        if (checkGood_String(holder)) {
+//         if (checkGood_String(holder)) {
            
-            if (isGreator(holder,max)) {
-                max = holder;
+//             if (isGreator(holder,max)) {
+//                 max = holder;
+//             }
+//         }
+//         left ++;
+//         right++;
+//     }
+
+//     return (max == "-111") ? "" : (max);
+// }
+
+    string largestGoodInteger(string num) {
+        //I keppt this in Decreasing order as we want max good string so we start checking from max
+        vector<string> good_strings ={"999","888","777","666","555","444","333","222","111","000"};
+
+        for(auto goodString:good_strings)
+        {
+            int left=0;
+            int right=2;
+
+            while(right<num.size())
+            {
+                string holder=num.substr(left,right-left+1);
+
+                if(holder==goodString)
+                {
+                    return holder;
+                }
+
+                right++;
+                left++;
             }
         }
-        left ++;
-        right++;
+        return "";
+    
     }
 
-    return (max == "-111") ? "" : (max);
-}
 };
