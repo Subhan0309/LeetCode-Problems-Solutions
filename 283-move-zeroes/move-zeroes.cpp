ceac82c23dad9bb669bae8875b4d74f2ecd6167b
Muhammad Subhan <138607323+Subhan0309@users.noreply.github.com>
2024-02-03 13:41:26 +0500

    Time: 34 ms (16.32%) | Memory: 21.5 MB (18.70%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int length=nums.size();
-        int zeroes=0;
-        for(auto num:nums)
+  void moveZeroes(vector<int>& nums) {
+    int length = nums.size();
+    int left = 0;
+
+    for (int i = 0; i < length; i++){
+        
+        if (nums[i] != 0)
         {
-            if(num==0)
-            {
-                zeroes++;
-            }
+            int temp = nums[left];
+            nums[left] = nums[i];
+            nums[i] = temp;
+
+            left++;
         }
-        if(length==zeroes)//means the array already contains all zeroes
-        {
-            return;
-        }
-
-        for(int i=0;i<length;i++)
-        {
-            if(nums[i]==0)
-            {
-                for(int j=i+1;j<length;j++) // loop to find the non-zero digit
-                {
-                    if(nums[j])
-                    {
-                        int temp=nums[j];
-                        nums[j]=nums[i];
-                        nums[i]=temp;
-                        break;
-                    }
-                }
-            }
-        }     
-
-        // if we found a zero , shift the whole array to left , and enter a zero at the end .  
-
+        
     }
+    
+    
+
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << nums[i] << " ";
+    }
+
+}
 };
