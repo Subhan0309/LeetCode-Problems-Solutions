8a79e048e2eeb0c8f381c4be740f848830bcc5b9
Muhammad Subhan <138607323+Subhan0309@users.noreply.github.com>
2025-08-30 13:28:08 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.38%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        return 1L * n * m / 2;
+        long long onePossibility = 1L * ((n+1)/2) * (m/2);
+        long long secondPossibility = 1L * (n/2) * ((m+1)/2);
+
+        // (x - odd, y - even) * (x - even, y - odd)
+        
+
+        return onePossibility + secondPossibility;
     }
 };
