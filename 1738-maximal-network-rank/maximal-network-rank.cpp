c044c1f4d42036b04d160d00d48888e622e50683
Muhammad Subhan <138607323+Subhan0309@users.noreply.github.com>
2026-06-20 16:54:21 +0500

    Time: 361 ms (5.05%) | Memory: 107.7 MB (5.05%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        
+
         map<vector<int>, bool> container;
         vector<int> degrees(n,0);
+        // storing edges presence in hash for faster lookup
         for(const auto& road: roads){
             container[road] = true;
         }
 
+        // calculating the degree for every node
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
 
@@ -17,6 +19,14 @@ public:
 
             }
         }
+
+        // while calculating maximum , we see degrees & presence of edge 
+        // two cities have maximum network rank if the sum of degrees of them is greator of  
+        // all pairs.
+        // Here is the catch , which is that 
+        // We need to minus the degree count from one node if both have edge between them
+        // while calculating the sum.
+
 
         int maximum = 0;
         for(int i = 0 ; i < n ; i++){
